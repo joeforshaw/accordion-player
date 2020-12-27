386129a5a4fc5eb236756ef11ea555d7a35486f7
Joe Forshaw <joe@joeforshaw.com>
2020-12-27 22:58:00 +0000

    WIP: auld lang syne data

--- data/auld_lang_syne.js
+++ data/auld_lang_syne.js
@@ -1,6 +1,38 @@
 export default [
-  { note: 'B', octave: 1, beats: 1 },
-  { note: 'C', octave: 2, beats: 1 },
-  { note: 'D', octave: 2, beats: 0.5 },
-  { note: 'E', octave: 2, beats: 0.5 },
+  // Bar 1
+  { note: 'D', octave: 2, beats: 1 },
+
+  // Bar 2
+  { note: 'G', octave: 2, beats: 1.5 },
+  { note: 'G', octave: 2, beats: 0.5 },
+  { note: 'G', octave: 2, beats: 1 },
+  { note: 'B', octave: 2, beats: 1 },
+
+  // Bar 3
+  { note: 'A', octave: 2, beats: 1.5 },
+  { note: 'G', octave: 2, beats: 0.5 },
+  { note: 'A', octave: 2, beats: 1 },
+  { note: 'B', octave: 2, beats: 1 },
+
+  // Bar 4
+  { note: 'G', octave: 2, beats: 1.5 },
+  { note: 'G', octave: 2, beats: 0.5 },
+  { note: 'B', octave: 2, beats: 1 },
+  { note: 'D', octave: 3, beats: 1 },
+
+  // Bar 5
+  { note: 'E', octave: 3, beats: 3 },
+  { note: 'E', octave: 3, beats: 1 },
+
+  // Bar 6
+
+  // Bar 7
+
+  // Bar 8
+
+  // Bar 9
+
+  // Bar 10
+
+  // Bar 11
 ]
